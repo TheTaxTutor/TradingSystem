6ad9933201693ea735cba8783888c04742860cfd
TheTaxTutor <Phillip.Dwyer@thetaxtutor.net>
2025-04-08 19:47:50 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,9 +5,9 @@ import os
 st.title("Hermetic Trading System")
 
 # Load your data
-data_folder = "data"
-cba_data = pd.read_csv(os.path.join(data_folder, "20250408_CBA_HISTORICAL DATA.csv"))
-stars_data = pd.read_csv(os.path.join(data_folder, "20250408_Phillip Dwyer_STAR CHARTS.csv"))
+data_folder = ".data"
+cba_data = pd.read_csv(os.path.join(data_folder, "20250408_CBA_HISTORICAL_DATA.csv"))
+stars_data = pd.read_csv(os.path.join(data_folder, "20250408_PhillipDwyer_STAR_CHARTS.csv"))
 
 # Preview data
 st.subheader("CBA Data")
